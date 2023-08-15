10ff929b580e8d0d4f51043793fdbc8fabb593d0
YuBeenSo <yso682879@gmail.com>
2023-08-15 14:02:57 +0900

    15일 마지막 수업

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("hello, javascript");
+
+// 변수 선언과 함수 작성
+var age = 25;
+let year = 2023;
+const point = 250; // 숫자로 인식
+const today = "광복절"; // 문자로 인식
+
+console.log(age);
+console.log(year);
+console.log(point);
+
+// const pass = false;
+const pass = true;
+
+if (pass) {
+  console.log("hello");
+}
+
+// data type (JS 뿐만 아니라 모든 프로그래밍 언어들이 가지고 있는 특성)
+// Number = 1, 2, 3, 4, 5, 7.5, 8.5 (int, float 이렇게 따로 분류하지 않고 한꺼번에 number로 분류)
+// String = 'hello world' (문자열, 모든 프로그래밍 언어 공통)
+// Boolean = ture/false (참, 거짓)
+// Null, Undefined, Object
